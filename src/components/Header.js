09321038ca88b9af08ce2e5eca221b10320d421d
Mohammad Monfared <monfared.mohammad@gmail.com>
2024-05-21 21:52:31 -0400

    1-Nav is done

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import bannerImg from '../images/restauranfood.jpg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <section>
+        {/* banner texts*/}
+        <div className="banner">
+          <h2>Little Lemon</h2>
+          <h3>Chicago</h3>
+          <p>
+            We are a family owned Mediterraneran returant, focused on
+            traditional recipes servred with a modern twist.
+          </p>
+          <Link to="/booking">
+            <button aria-label="On Click">Reserve Table</button>
+          </Link>
+        </div>
+
+        {/* banner texts*/}
+        <div>
+          <img src={bannerImg} alt="" />
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
