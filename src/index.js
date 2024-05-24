5a4aeb62385e67f1a239b5be57de458ca2ba38b6
Mohammad Monfared <monfared.mohammad@gmail.com>
2024-05-23 21:08:59 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
+    <BrowserRouter basename="/meta-app-project">
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
